ebd5fb86ea41db04264277eb96765cf47d3cfd7a
JMsuper <tjsqls2067@naver.com>
2023-12-26 21:54:18 +0900

    delete member package 로컬 환경설정을 위해 임시 제거

--- banking/src/main/java/com/bankingsystem/banking/member/service/MemberService.java
+++
@@ -1,9 +0,0 @@
-package com.bankingsystem.banking.member.service;
-
-import com.bankingsystem.banking.member.dto.MemberDto;
-
-public interface MemberService {
-    public boolean joinUp(MemberDto dto);
-
-    public boolean isIdDuplicate(String memberId);
-}
